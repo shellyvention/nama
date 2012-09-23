6286ce01880bd539aff237590c24779d253ddd05
Sandy Freihofer <sandra.freihofer@gmail.com>
2012-09-23 22:00:45 +0200

    Initial import
    - Generated new rails app
    - Customized readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Not another Membership administration (NaMa)
+
+## General information
+
+This is the ZHAW semester project from Sandra Freihofer. It is a web application
+developed with Rails.
+
+Additional information will follow asap.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nama::Application.config.secret_token = '7e111e90e18c494b2763657946c8363e31497bbc2eeca706e88a57628d8c277fd47c3a1b4e235eca7f0f6dee6f61b8f1810cb4eadaba25f206117125e823bfbe'
