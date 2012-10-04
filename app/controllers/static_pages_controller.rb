e7942c1dc2577e8f1b4ca14181bb6df8dc3ea036
Sandy Freihofer <sandra.freihofer@gmail.com>
2012-10-05 00:46:00 +0200

    Add new actions to StaticPages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,13 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+
+  def signin
+  end
+
+  def about
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Nama::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-
   resources :users
+
+  root to: 'static_pages#home'
+
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/help', to: 'static_pages#help'
+  match '/signin', to: 'static_pages#signin'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
