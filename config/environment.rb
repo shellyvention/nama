4f20a801d6c0911a89c9d91476cbc51e883f408d
Sandra Freihofer <sandra.freihofer@gmail.com>
2012-10-23 12:56:16 +0200

    Use custom date format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Nama::Application.initialize!
+
+# Custom Date Format
+Date::DATE_FORMATS.merge!(:default => "%d.%m.%Y")
