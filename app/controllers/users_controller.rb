8a6913833579c1cb19c3cf86ed30c3034c6cedb1
Sandra Freihofer <sandra.freihofer@gmail.com>
2012-12-06 22:47:53 +0100

    Authorize user to view his own profile, change welcome text in home pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authorize_admin, except: [:create_activation, :activate_user]
+  before_filter :authorize_admin, except: [:show, :create_activation, :activate_user]
 
   def index
     @users = User.all
