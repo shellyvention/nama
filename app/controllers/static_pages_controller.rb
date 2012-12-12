a209f7368b2568c9eb32f2e440fe6d3b63040511
Sandra Freihofer <sandra.freihofer@gmail.com>
2012-12-13 00:56:47 +0100

    Small overall view for admins

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,6 +12,12 @@ class StaticPagesController < ApplicationController
   end
 
   def home_admin
+    @num_members = User.where("email != \'nama\'").count
+    @num_groups = Group.count
+    @num_males = User.where("gender == 0").count
+    @num_females = User.where("gender == 1").count
+    @num_upcoming_events = Event.upcoming.count
+    @num_past_events = Event.past.count
   end
 
   def home_user
