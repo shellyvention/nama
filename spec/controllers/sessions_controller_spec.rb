f7f125e43a1311e6a7d760407aef418e158e04c8
Sandra Freihofer <sandra.freihofer@gmail.com>
2012-12-19 23:21:09 +0100

    Specs for sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,95 @@ require 'spec_helper'
 
 describe SessionsController do
 
+  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:admin) }
+  let(:inactive) { FactoryGirl.create(:inactive) }
+
+  describe "new" do
+    context "not logged in" do
+      before do
+        User.current = nil
+      end
+
+      it "renders the new template" do
+        get :new
+        response.should render_template("new")
+      end
+    end
+
+    context "logged in user" do
+      before do
+        User.current = user
+      end
+
+      it "redirects to home_user" do
+        get :new
+        response.should redirect_to(home_user_url)
+      end
+
+    end
+
+    context "logged in admin" do
+      before do
+        User.current = admin
+      end
+
+      it "redirects to home_admin" do
+        get :new
+        response.should redirect_to(home_admin_url)
+      end
+    end
+  end
+
+  describe "create" do
+    before  do
+      User.current = nil
+    end
+
+    context "admin" do
+      it "redirects to home_admin" do
+        post :create, session: { email: admin.email, password: "ThePassword-1" }
+        response.should redirect_to(home_admin_url)
+        User.current.should eq(admin)
+      end
+    end
+
+    context "user" do
+      it "redirects to home_user" do
+        post :create, session: { email: user.email, password: "ThePassword-1" }
+        response.should redirect_to(home_user_url)
+        User.current.should eq(user)
+      end
+    end
+
+    context "inactive" do
+      it "rendes the new template with error" do
+        post :create, session: { email: inactive.email, password: "ThePassword-1" }
+        flash[:error].should eq("Your user account is disabled.
+          Please contact the administrator.")
+        response.should render_template("new")
+        User.current.should eq(nil)
+      end
+    end
+
+    context "user does not exist" do
+      it "rendes the new template with error" do
+        post :create, session: { email: 'not@existing.com' }
+        flash[:error].should eq("Invalid email/password combination.")
+        response.should render_template("new")
+      end
+    end
+  end
+
+  describe "destroy" do
+    before do
+      User.current = user
+    end
+
+    it "signs out" do
+      delete :destroy
+      response.should redirect_to(root_url)
+      User.current.should eq(nil)
+    end
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -12,6 +12,7 @@ FactoryGirl.define do
     password		        "ThePassword-1"
     password_confirmation	"ThePassword-1"
     role			        0
+    active			        1
   end
 
   factory :admin, class: User do
@@ -26,6 +27,7 @@ FactoryGirl.define do
     password		        "ThePassword-1"
     password_confirmation	"ThePassword-1"
     role			        1
+    active			        1
   end
 
   factory :organizer, class: User do
@@ -40,7 +42,24 @@ FactoryGirl.define do
     password		        "ThePassword-1"
     password_confirmation	"ThePassword-1"
     role			        2
+    active			        1
   end
+
+  factory :inactive, class: User do
+    last_name		        "Muster"
+    first_name		        "Peter"
+    street			        "Musterstrasse 1"
+    postal_code		        "1111"
+    city			        "Musterstadt"
+    date_of_birth	        { 40.years.ago }
+    gender			        0
+    email			        "peter.locked@muster.ch"
+    password		        "ThePassword-1"
+    password_confirmation	"ThePassword-1"
+    role			        2
+    active			        0
+  end
+
 
   factory :event do
     name        "MyEvent"
